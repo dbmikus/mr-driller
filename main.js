b7524354ce579fbf54cbeb19d35c6cc43f8abe6d
Dylan Mikus <dbmikus@gmail.com>
2013-01-21 18:05:31 -0500

    Starting to draw objects on the screen

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
+// Global variables
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
 function main() {
-    alert("Hi Nathan");
+    drawDisplay();
+
+    var driller = new Driller(100, 100);
+    console.log(driller);
+}
+
+function Driller(startX, startY) {
+    this.xPos = startX;
+    this.yPos = startY;
+
+    this.move = function (dx, dy) {
+        this.xPos += dx;
+        this.yPos += dy;
+    }
+
+    window.ctx.fillRect(this.xPos, this.yPos, 50, 50);
+}
+
+function drawScoreboard(width, height) {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width - width, 0);
+    // Drawing vertical divider line
+    ctx.lineTo(canvas.width - width, height);
+    ctx.stroke();
+}
+
+function drawWorld(width, height) {
+    console.log("Not implemented.");
+}
+
+function drawDisplay() {
+    worldWidth = 450;
+    drawScoreboard(canvas.width - worldWidth, canvas.height);
+    drawWorld(worldWidth, canvas.height);
 }
 
 main();
