5542d81fb4e6d0870a3d1f24e7909fa2a3c4141a
Dylan Mikus <dbmikus@gmail.com>
2013-01-27 18:03:22 -0500

    Blocks now shake and fall on a delay. Need animation and maybe some tweaks with drilling

--- blocks.js
+++ blocks.js
@@ -1,9 +1,27 @@
 // Contains block functions, but no global variables
 
-//Block object
-function Block(type){
+// Block object
+// The state parameter is optional
+// Block states are:
+//   stationary
+//   shaking
+//   falling
+function Block(type, state){
+    var countdownFactor = 3;
+
     //string describing the content of the block
     this.type = type;
+
+    if (state === undefined) {
+        state = "stationary";
+    }
+    this.state = state;
+    this.countdown = countdownFactor;
+
+    this.changeState = function (newState) {
+        this.countdown = countdownFactor;
+        this.state = newState;
+    }
 }
 
 // Given a position and a type of block, recursively finds a connected group of
@@ -91,39 +109,67 @@ function blockGravity(blocks) {
         }
     }
 
-    // Move all blocks that fall down by one.
-    // If they are on the bottom and are falling, just forget about them. This
-    // should only happen to empty blocks on the bottom row, which I'm pretty
-    // sure we won't spawn, but it's better to be safe than sorry.
-    // We start on the second row because the bottom row cannot move down
-    // any further.
-    // We start at the bottom and work our way up so we do not overwrite
-    // anything.
+    blocks = blockGravityMove(blocks, checkedGrid);
+    return {"statusGrid": checkedGrid, "blockGrid": blocks};
+}
+
+// Move all blocks that fall down by one.
+// If they are on the bottom and are falling, just forget about them. This
+// should only happen to empty blocks on the bottom row, which I'm pretty
+// sure we won't spawn, but it's better to be safe than sorry.
+// We start on the second row because the bottom row cannot move down
+// any further.
+// We start at the bottom and work our way up so we do not overwrite
+// anything.
+function blockGravityMove(blocks, checkedGrid) {
     for (y=1; y<maxRows; y++) {
         for(x=0; x<blockcolumns; x++) {
-            // if (blocks[x][y].type !== "empty"
-            //     && checkedGrid[x][y] === "falls") {
             // Empty blocks only fall downwards if they are falling into a space
             // that will become empty.
             // TODO There might be a cleaner way to do this.
             if (checkedGrid[x][y] === "falls") {
                 if (blocks[x][y].type === "empty") {
                     if (checkedGrid[x][y-1] === "falls") {
-                        blocks[x][y-1] = blocks[x][y];
+//                        blocks[x][y-1] = blocks[x][y];
                     }
                 }
+                // The block is not an empty block
                 else {
-                    blocks[x][y-1] = blocks[x][y];
+                    if (blocks[x][y].state === "stationary") {
+                        blocks[x][y].changeState("shaking");
+                    }
+                    else if (blocks[x][y].state === "shaking") {
+                        if (blocks[x][y].countdown === 0) {
+                            blocks[x][y].changeState("falling");
+                        } else {
+                            blocks[x][y].countdown -= 1;
+                        }
+                    }
+                    else if (blocks[x][y].state === "falling") {
+                        if (blocks[x][y].countdown === 0) {
+                            // call this to reset countdown
+                            blocks[x][y].changeState("falling");
+                            blocks[x][y-1] = blocks[x][y];
+                            blocks[x][y] = new Block("empty");
+                        } else {
+                            blocks[x][y].countdown -= 1;
+                        }
+                    }
                     // If something would fall into the top row, just make it
                     // empty. This way we don't have a forever falling column
-                    if (y === maxRows-1 || checkedGrid[x][y+1] === "stays")
-                        blocks[x][y] = new Block("empty");
+                    if (y === maxRows-1 || checkedGrid[x][y+1] === "stays") {
+//                        blocks[x][y] = new Block("empty");
+                    }
                 }
+            }
+            // The block does not fall, set its state to stationary
+            else {
+                blocks[x][y].changeState("stationary");
             }
         }
     }
 
-    return {"statusGrid": checkedGrid, "blockGrid": blocks};
+    return blocks;
 }
 
 // Expects a valid block grouping.
